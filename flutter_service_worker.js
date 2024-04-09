2a26dc22c19d49eb8e95b6c0d6eab0394bcb91af
Christian Kousholt <kousholt@mcaoptimizers.com>
2024-04-09 23:04:34 +0200

    CRUD functions for TimeRegistrations hooked up

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,9 +38,9 @@ const RESOURCES = {"assets/asset/gifs/handshake.gif": "b5ff0749fa806ee7a8a1f71e3
 "icons/Icon-512.png": "8422469a62c39c2036d29b0bc9f63348",
 "icons/Icon-maskable-192.png": "6486752fd54c9c090bbeeea60b69bf8d",
 "icons/Icon-maskable-512.png": "8422469a62c39c2036d29b0bc9f63348",
-"index.html": "6223422be8ff05eb9ba989693973c7ff",
-"/": "6223422be8ff05eb9ba989693973c7ff",
-"main.dart.js": "e8f35dd49db94e3be6503c21008b021f",
+"index.html": "5e19aa5868b4b414270e5213ab6f29ff",
+"/": "5e19aa5868b4b414270e5213ab6f29ff",
+"main.dart.js": "70b749678b2dc8617788150c33d18bb7",
 "manifest.json": "c22ee867716e6e04b22e564a0d7b1744",
 "splash/img/light-background.png": "bbe2c61715551ac7be4008131454a2b0",
 "version.json": "9b5adf539d3391529d09afef88ef7ccb"};
