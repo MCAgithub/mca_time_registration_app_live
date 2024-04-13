b727d53bb17c8da1101add43031454b49850067f
Christian Kousholt <kousholt@mcaoptimizers.com>
2024-04-13 14:17:22 +0200

    recreated the webfolder and forgot the whitescreen bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,8 +40,8 @@ const RESOURCES = {"assets/asset/gifs/handshake.gif": "b5ff0749fa806ee7a8a1f71e3
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0aa07a18734227d8ed70892fd47583c4",
-"/": "0aa07a18734227d8ed70892fd47583c4",
+"index.html": "07e5f522441ca14cb39bb162e1a5a827",
+"/": "07e5f522441ca14cb39bb162e1a5a827",
 "main.dart.js": "2ab3af514326e081e0751807ee57dca3",
 "manifest.json": "f0eada3909340a19f214675b60870df3",
 "version.json": "9b5adf539d3391529d09afef88ef7ccb"};
